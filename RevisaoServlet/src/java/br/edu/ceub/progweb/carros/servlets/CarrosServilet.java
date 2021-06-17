eaca705b055dd425a20342c12ec8f40b535363b5
Matheus Baptista Rodrigues <matheusim27@hotmail.com>
2021-06-17 16:27:11 -0300

    Create web.xml

---
+++ RevisaoServlet/src/java/br/edu/ceub/progweb/carros/servlets/CarrosServilet.java
@@ -0,0 +1,22 @@
+package br.edu.ceub.progweb.carros.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "CarrosServilet", urlPatterns = {"/carrosservlet"})
+public class CarrosServilet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
---
+++ RevisaoServlet/web/jsp/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : 31/05/2021, 19:29:34
+    Author     : david
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Cadastro de carros - Carros S.A</title>
+    </head>
+    <body>
+        <h1>Cadastro de carros - Carros S.A</h1>
+        <a href="../carros"></a>
+    </body>
+</html>
